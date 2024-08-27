b5600450833c210b95ae36750763045054a571b5
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-08-28 00:22:30 +0300

    Add latesCollection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const LatestCollection = () => {
-  return <div>LatestCollection</div>;
+  const { products } = useContext(ShopContext);
+
+  console.log(products);
+
+  return <div></div>;
 };
 
 export default LatestCollection;
