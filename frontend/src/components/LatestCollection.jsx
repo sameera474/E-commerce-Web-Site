ef5fb036e71af1c1bfcea8727d079ee7d276d726
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-08-29 01:23:33 +0300

    Add some wordings

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
 
-  console.log(products);
-
-  return <div></div>;
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600"></p>
+      </div>
+    </div>
+  );
 };
 
 export default LatestCollection;
--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Title = ({ text1, texy2 }) => {
+const Title = ({ text1, text2 }) => {
   return (
     <div className="inline-flex gap-2 items-center mb-3">
       <p className="text-gray-500">
