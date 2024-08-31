d6ad1b784e0af531b3c412fee9961fe9d8690c61
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-08-31 18:03:25 +0300

    Add comment

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -9,7 +9,10 @@ const LatestCollection = () => {
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTION"} />
-        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600"></p>
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem Ipsum is simple dummy text of the printing and typesetting
+          industry. lorem Ipsum has been the.
+        </p>
       </div>
     </div>
   );
